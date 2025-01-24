b56b75efee7936391dcbad73f7baff5f03e7f145
phuocnehihi1 <vohuuphuoc1102@gmail.com>
2025-01-24 14:40:00 +0700

    CommitLazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultBodyComponent } from './core/layouts/default-body/default-body.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { ProductComponent } from './features/dashboard/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DefaultBodyComponent,
+  },
+  {
+    path: 'product',
+    component: ProductComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+//   {
+//     path: 'products',
+//     data: {
+//       title: 'Product',
+//     },
+//     children: [
+//       {
+//         path: '',
+//         loadChildren: () =>
+//           import('./features/dashboard/dashboard.module').then(
+//             (m) => m.DashboardModule
+//           ),
+//       },
+//     ],
+//   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AlertModule } from '@coreui/angular';
+import { HttpClientModule } from '@angular/common/http'; //
+
+import { FormatDraftPipe } from './shared/piges/format-draft.pipe';
+import { DashboardModule } from './features/dashboard/dashboard.module';
+import { AuthModule } from './features/auth/auth.module';
+import { LayoutsModule } from './core/layouts/layouts.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FormatDraftPipe],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DashboardModule,
+    AuthModule,
+    AlertModule,
+    HttpClientModule,
+    LayoutsModule,
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
